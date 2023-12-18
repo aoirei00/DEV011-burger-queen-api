2030b76a4f37cc7a2845ceb7c4fca6d95a89a0a4
aoirei00 <aoirei02@gmail.com>
2023-12-18 08:44:55 -0600

    Add: products controllers

---
+++ controller/products.js
@@ -0,0 +1,154 @@
+const { ObjectId } = require('mongodb');
+const { connect } = require('../connect');
+
+module.exports = {
+  /// Obtener listade de todos los productos
+  getProducts: async (req, resp, next) => {
+    try {
+      const db = await connect();
+      const productsCollection = db.collection('products');
+      const products = await productsCollection.find().toArray();
+
+      console.log(products);
+      resp.json(products);
+    } catch (error) {
+      console.error(error);
+      resp.status(500).json({ error: 'Error al obtener la lista de productos' });
+    }
+  },
+  /// Obtener pruducto por id
+  getProductsUid: async (req, resp, next) => {
+    try {
+      const db = await connect();
+      const productsCollection = db.collection('products');
+      const productId = req.params.productId;
+
+      // Verifica si el ID proporcionado es válido
+      if (!ObjectId.isValid(productId)) {
+        console.log('ID de producto no válido');
+        return resp.status(400).json({ error: 'ID de producto no válido' });
+      }
+
+      // Convierte el ID en ObjectId
+      const objectId = new ObjectId(productId);
+
+      // Encuentra el usuario por su ID
+      const product = await productsCollection.findOne({ _id: objectId });
+
+      if (!product) {
+        console.log('El producto no existe');
+        return resp.status(404).json({ error: 'El producto no existe' });
+      }
+
+      console.log(product);
+      resp.json(product);
+    } catch (error) {
+      console.error(error);
+      resp.status(500).json({ error: 'Error al obtener el producto' });
+    }
+  },
+
+  /// Crear nuevos productos
+  postProducts: async (req, resp, next) => {
+    try {
+      const db = await connect();
+      const productsCollection = db.collection('products');
+      const existingproduct = await productsCollection.findOne({ name: req.body.name });
+      if (existingproduct) {
+        console.log('El producto ya existe');
+        return resp.status(400).json({ error: 'El producto ya existe' });
+      }
+      const newProduct = {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        type: req.body.type,
+      };
+      await productsCollection.insertOne(newProduct);
+      console.log('Se agrego el producto con exito!!');
+      resp.status(201).json(newProduct);
+    } catch (error) {
+      console.log('Error al agregar producto');
+      resp.status(500).json({ error: 'Error al agregar un nuevo producto' });
+    }
+  },
+  /// Actualiza o edita al producto
+  updateProductsUid: async (req, resp, next) => {
+    try {
+      const db = await connect();
+      const productsCollection = db.collection('products');
+      const productId = req.params.productId;
+
+      // Verifica si el ID proporcionado es válido
+      if (!ObjectId.isValid(productId)) {
+        console.log('ID de prosucto no válido');
+        return resp.status(400).json({ error: 'ID de producto no válido' });
+      }
+
+      // Convierte el ID en ObjectId
+      const objectId = new ObjectId(productId);
+
+      // Encuentra el producto por su ID
+      const product = await productsCollection.findOne({ _id: objectId });
+
+      if (!product) {
+        console.log('El producto no existe');
+        return resp.status(404).json({ error: 'El producto no existe' });
+      }
+      const updatedData = {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        type: req.body.type,
+      };
+
+      // Realiza la actualización del producto
+      const result = await productsCollection.updateOne({ _id: objectId }, { $set: updatedData });
+
+      if (result.modifiedCount === 1) {
+        console.log('producto actualizado con éxito');
+        resp.json({ message: 'producto actualizado con éxito' });
+      } else {
+        console.log('No se pudo actualizar el producto');
+        resp.status(500).json({ error: 'No se pudo actualizar el producto' });
+      }
+    } catch (error) {
+      console.error(error);
+      resp.status(500).json({ error: 'Error al obtener el producto' });
+    }
+  },
+
+  /// Elimina el producto
+  deleteProductsUid: async (req, resp, next) => {
+    try {
+      const db = await connect();
+      const productsCollection = db.collection('products');
+      const productId = req.params.productId;
+
+      // Verifica si el ID proporcionado es válido
+      if (!ObjectId.isValid(productId)) {
+        console.log('ID de producto no válido');
+        return resp.status(400).json({ error: 'ID de producto no válido' });
+      }
+
+      // Convierte el ID en ObjectId
+      const objectId = new ObjectId(productId);
+
+      // Encuentra el producto por su ID
+      const product = await productsCollection.findOne({ _id: objectId });
+      if (!product) {
+        console.log('El producto no existe');
+        return resp.status(404).json({ error: 'El producto no existe' });
+      }
+
+      await productsCollection.deleteOne({ _id: objectId });
+
+      console.log('Producto eliminado con éxito');
+      resp.status(200).json({ message: 'Producto eliminado con éxito' });
+    } catch (error) {
+      console.error(error);
+      resp.status(500).json({ error: 'Error al eliminar el Producto' });
+    }
+  },
+
+};
--- routes/products.js
+++ routes/products.js
@@ -3,21 +3,29 @@ const {
   requireAdmin,
 } = require('../middleware/auth');
 
+const {
+  getProducts,
+  getProductsUid,
+  postProducts,
+  updateProductsUid,
+  deleteProductsUid,
+} = require('../controller/products');
+
 module.exports = (app, nextMain) => {
-  app.get('/products', requireAuth, (req, resp, next) => {
-    resp.json({ msg: ' OK ' });
+  app.get('/products', requireAuth, getProducts, (req, resp, next) => {
+    // resp.json({ msg: ' OK ' });
   });
 
-  app.get('/products/:productId', requireAuth, (req, resp, next) => {
+  app.get('/products/:productId', requireAuth, getProductsUid, (req, resp, next) => {
   });
 
-  app.post('/products', requireAdmin, (req, resp, next) => {
+  app.post('/products', requireAdmin, postProducts, (req, resp, next) => {
   });
 
-  app.put('/products/:productId', requireAdmin, (req, resp, next) => {
+  app.put('/products/:productId', requireAdmin, updateProductsUid, (req, resp, next) => {
   });
 
-  app.delete('/products/:productId', requireAdmin, (req, resp, next) => {
+  app.delete('/products/:productId', requireAdmin, deleteProductsUid, (req, resp, next) => {
   });
 
   nextMain();
